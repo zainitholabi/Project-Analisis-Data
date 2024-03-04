7916a331d503df6923d70e2db032e603d1a14e3a
zainitholabi <ahmadzaini1908@gmail.com>
2024-03-04 23:46:56 +0700

    Update main.py

--- dashboard/main.py
+++ dashboard/main.py
@@ -56,10 +56,10 @@ state, most_common_state = function.createDataFrameBystate()
 order_status, common_status = function.createOrderStatus()
 
 # Title
-st.header("E-Commerce Dashboard :convenience_store:")
+st.header("Dashboard E-Commerce")
 
 # Daily Orders
-st.subheader("Daily Orders")
+st.subheader("Orders Harian")
 
 col1, col2 = st.columns(2)
 
@@ -69,7 +69,7 @@ with col1:
 
 with col2:
     total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Revenue: **{total_revenue}**")
+    st.markdown(f"Total Pendapatan: **{total_revenue}**")
 
 fig, ax = plt.subplots(figsize=(12, 6))
 ax.plot(
@@ -84,16 +84,16 @@ ax.tick_params(axis="y", labelsize=15)
 st.pyplot(fig)
 
 # Customer Spend Money
-st.subheader("Customer Spend Money")
+st.subheader("Pembelanjaan Uang Pelanggan")
 col1, col2 = st.columns(2)
 
 with col1:
     total_spend = format_currency(sum_spend_df["total_spend"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Spend: **{total_spend}**")
+    st.markdown(f"Total Pembelanjaan: **{total_spend}**")
 
 with col2:
     avg_spend = format_currency(sum_spend_df["total_spend"].mean(), "IDR", locale="id_ID")
-    st.markdown(f"Average Spend: **{avg_spend}**")
+    st.markdown(f"Rata - Rata Pembelanjaan: **{avg_spend}**")
 
 fig, ax = plt.subplots(figsize=(12, 6))
 ax.plot(
@@ -108,7 +108,7 @@ ax.tick_params(axis="y", labelsize=15)
 st.pyplot(fig)
 
 # Order Items
-st.subheader("Order Items")
+st.subheader("Items Order")
 col1, col2 = st.columns(2)
 
 with col1:
@@ -117,7 +117,7 @@ with col1:
 
 with col2:
     avg_items = sum_order_items_df["product_count"].mean()
-    st.markdown(f"Average Items: **{avg_items}**")
+    st.markdown(f"Rata - Rata Items: **{avg_items}**")
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
 
@@ -125,14 +125,14 @@ colors = ["#068DA9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
 sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_xlabel("Jumlah Penjualan", fontsize=30)
 ax[0].set_title("Produk paling banyak terjual", loc="center", fontsize=50)
 ax[0].tick_params(axis ='y', labelsize=35)
 ax[0].tick_params(axis ='x', labelsize=30)
 
 sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].set_xlabel("Jumlah Penjualan", fontsize=30)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
@@ -143,16 +143,16 @@ ax[1].tick_params(axis='x', labelsize=30)
 st.pyplot(fig)
 
 # Review Score
-st.subheader("Review Score")
+st.subheader("Skor Ulasan")
 col1,col2 = st.columns(2)
 
 with col1:
     avg_review_score = review_score.mean()
-    st.markdown(f"Average Review Score: **{avg_review_score}**")
+    st.markdown(f"Rata - Rata Skor Ulasan: **{avg_review_score}**")
 
 with col2:
     most_common_review_score = review_score.value_counts().index[0]
-    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
+    st.markdown(f"Skor Ulasan Paling Umum: **{most_common_review_score}**")
 
 fig, ax = plt.subplots(figsize=(12, 6))
 sns.barplot(x=review_score.index, 
@@ -161,19 +161,19 @@ sns.barplot(x=review_score.index,
             palette=["#068DA9" if score == common_score else "#D3D3D3" for score in review_score.index]
             )
 
-plt.title("Rating by customers for service", fontsize=15)
+plt.title("Peringkat dari pelanggan untuk layanan", fontsize=15)
 plt.xlabel("Rating")
-plt.ylabel("Count")
+plt.ylabel("Jumlah")
 plt.xticks(fontsize=12)
 st.pyplot(fig)
 
 # Customer Demographic
-st.subheader("Customer Demographic")
-tab1, tab2, tab3 = st.tabs(["State", "Order Status", "Geolocation"])
+st.subheader("Demografi Pelanggan")
+tab1, tab2, tab3 = st.tabs(["Negara Bagian", "Status Order", "Geolokasi"])
 
 with tab1:
     most_common_state = state.customer_state.value_counts().index[0]
-    st.markdown(f"Most Common State: **{most_common_state}**")
+    st.markdown(f"Negara Bagian Paling Umum: **{most_common_state}**")
 
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=state.customer_state.value_counts().index,
@@ -182,15 +182,15 @@ with tab1:
                 palette=["#068DA9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
                     )
 
-    plt.title("Number customers from State", fontsize=15)
-    plt.xlabel("State")
-    plt.ylabel("Number of Customers")
+    plt.title("Jumlah Pelanggan dari Negara Bagian", fontsize=15)
+    plt.xlabel("Negara Bagian")
+    plt.ylabel("Jumlah Pelanggan")
     plt.xticks(fontsize=12)
     st.pyplot(fig)
 
 with tab2:
     common_status_ = order_status.value_counts().index[0]
-    st.markdown(f"Most Common Order Status: **{common_status_}**")
+    st.markdown(f"Status Order Paling Umum: **{common_status_}**")
 
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=order_status.index,
@@ -199,9 +199,9 @@ with tab2:
                 palette=["#068DA9" if score == common_status else "#D3D3D3" for score in order_status.index]
                 )
     
-    plt.title("Order Status", fontsize=15)
+    plt.title("Status Order", fontsize=15)
     plt.xlabel("Status")
-    plt.ylabel("Count")
+    plt.ylabel("Jumlah")
     plt.xticks(fontsize=12)
     st.pyplot(fig)
 
